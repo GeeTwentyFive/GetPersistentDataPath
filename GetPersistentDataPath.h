f8a08d2b9cdd4774648fa3f552f0fb33f394eaf9
Gee <59966523+GeeTwentyFive@users.noreply.github.com>
2024-12-27 18:45:56 +0100

    Create GetPersistentDataPath.h

---
+++ GetPersistentDataPath.h
@@ -0,0 +1,24 @@
+#ifndef _GET_PERSISTENT_DATA_PATH_H
+#define _GET_PERSISTENT_DATA_PATH_H
+
+#include <stdlib.h>
+
+
+
+char *GetPersistentDataPath() {
+
+        #if defined(_WIN32)
+                return getenv("APPDATA");
+        #elif defined(__linux__)
+                return getenv("XDG_CONFIG_HOME");
+        #elif defined(__APPLE__)
+                return getenv("HOME");
+        #else
+                return "";
+        #endif
+
+}
+
+
+
+#endif // _GET_PERSISTENT_DATA_PATH_H
